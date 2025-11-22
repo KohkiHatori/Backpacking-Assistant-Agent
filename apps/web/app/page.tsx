c59928eab99b6d86a417174956190fe853ff692e
kohkhatori <kouki20040124hatori@gmail.com>
2025-11-22 12:10:53 -0500

    fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -11,7 +11,6 @@ export default async function Home() {
     redirect("/auth");
   }
 
-  // @ts-expect-error - extending default session type
   const trips = await getUserTrips(session.user.id);
 
   return <Dashboard trips={trips} user={session.user} />;
